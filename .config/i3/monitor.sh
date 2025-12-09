fce3c48c0be65db8b2e369161de3bf298a3f71c6
leoaugustogs <leo.augustogs@gmail.com>
2025-12-08 21:09:19 -0400

    ignore

--- .config/i3/monitor.sh
+++ .config/i3/monitor.sh
@@ -1,3 +1,3 @@
-xrandr --output DisplayPort-0 --mode 1920x1080 --rate 144.00
-xrandr --output HDMI-A-0 --mode 1920x1080 --rate 239.76 --right-of DisplayPort-0
-xrandr --output HDMI-A-1 --mode 3840x2160 --rate 30.00 --right-of HDMI-A-0
+xrandr --output DisplayPort-0 --mode 1920x1080 --rate 144.00 --scale 1.0x1.0
+xrandr --output HDMI-A-0 --mode 1920x1080 --rate 239.76 --right-of DisplayPort-0 --scale 1.0x1.0
+# xrandr --output HDMI-A-1 --mode 3840x2160 --rate 30.00 --right-of HDMI-A-0 --scale 0.5x0.5
